e7dc38115e1df7daaa4f3d68268ca9665aceffe0
HarshalRathore <76205196+HarshalRathore@users.noreply.github.com>
2021-12-03 18:19:31 +0530

    Update shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/sh
 
-yes y | sudo pacman -S --needed git base-devel
-
-#ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-exit
-#starshipt prompt
-sh -c "$(curl -fsSL https://starship.rs/install.sh)"
-
-#auto-completion
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-#syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo pacman -S --needed git base-devel
 
 git clone https://github.com/HarshalRathore/my-i3-config.git
 cd my-i3-config
@@ -25,35 +13,46 @@ cp -R .fonts ~/
 cp .bashrc ~/
 cp .zshrc ~/
 cp -R .local ~/
+cp harshal/robbyrussell.zsh-theme ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
 
-mv harshal/.robbyrussell.zsh-theme harshal/robbyrussell.zsh-theme
-cp harshal/.robbyrussell.zsh-theme ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
+sudo pacman -S --needed - < pacman_pkgs
+
+#ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#starshipt prompt
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+#auto-completion
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 wget https://raw.githubusercontent.com/endeavouros-team/EndeavourOS-packages-lists/master/i3
 yes y | sudo pacman -S --needed - < i3
 
-if [[ $(cat /etc/*-release | awk -F '=' '{if(NR==2)print $2 }') != "EndeavourOS" ]]; then
-sudo echo "
-[endeavouros]
-SigLevel = PackageRequired
-Include = /etc/pacman.d/endeavouros-mirrorlist" >> /etc/pacman.conf
-sudo cp -R pacman.d /etc/
-fi
+#if [[ $(cat /etc/*-release | awk -F '=' '{if(NR==2)print $2 }') != "EndeavourOS" ]]; then
+#sudo echo "
+#[endeavouros]
+#SigLevel = PackageRequired
+#Include = /etc/pacman.d/endeavouros-mirrorlist" >> /etc/pacman.conf
+#sudo cp -R pacman.d /etc/
+#fi
 
-echo "Trying to install yay-AUR helper from EndevourOS"
+echo "Trying to install yay-AUR helper from pacman"
 yes y | sudo pacman -S --needed yay
 if [[ -f "/usr/bin/yay" ]]; then
 echo "Yet-Another-Yourt sucessfully Installed"
 else
-echo "Failed to Installed yay with with EndevourOS repository"
+echo "Failed to Installed yay with with pacman"
 echo "Beginning to install yay from source"
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+makepkg -sci
 cd ..
 fi
 
-yes y | sudo pacman -S --needed - < pacman_pkgs
 
 cd $config_path
 #pwd=$(pwd)
